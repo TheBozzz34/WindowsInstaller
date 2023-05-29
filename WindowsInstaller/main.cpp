7ba0d4c79cf1e83039342d7ab83022432c482bfd
Ethan James <lskywalker416@gmail.com>
2023-05-29 12:42:49 -0600

    Add License Text Add Default Window Size Prevent Resizing Add Install And Set Location Buttons

--- WindowsInstaller/main.cpp
+++ WindowsInstaller/main.cpp
@@ -18,6 +18,13 @@ private:
     void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
+    void OnInstallButtonClick(wxCommandEvent& event);
+    void OnSetButtonClick(wxCommandEvent& event);
+
+    wxButton* install_button;
+    wxButton* set_location;
+
+    wxTextCtrl* textCtrl;
 };
 
 bool WindowsInstaller::OnInit()
@@ -30,11 +37,15 @@ bool WindowsInstaller::OnInit()
 
 enum
 {
-    ID_Hello = 1
+    ID_Hello = 1,
+    ID_Install_Button = 2,
+    ID_Set_Location = 3
+
 };
 
+
 NewFrame::NewFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World")
+    : wxFrame(nullptr, wxID_ANY, "Hello World", wxDefaultPosition, wxSize(700, 500), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
 {
     wxMenu* menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl+H",
@@ -54,9 +65,38 @@ NewFrame::NewFrame()
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
 
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+
+    textCtrl = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY | wxHSCROLL);
+    textCtrl->SetBackgroundColour(*wxWHITE);
+    textCtrl->SetForegroundColour(*wxBLACK);
+    textCtrl->SetFont(wxFont(wxFontInfo(10)));
+
+    wxString licenseText = "Copyright 2023 Ethan James \n\n Permission is hereby granted, free of charge, to any person obtaining a copy of this software and \n associated documentation files(the “Software”), to deal in the Software without restriction, \n including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, \n and /or sell copies of the Software, and to permit persons to whom the Software is furnished to do \n so, subject to the following conditions : \n The above copyright notice and this permission notice shall be included in all copies or substantial \n portions of the Software. \n\n THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR \n IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS \n FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS \n OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, \n WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN \n CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+    textCtrl->SetValue(licenseText);
+
+    mainSizer->Add(textCtrl, 1, wxEXPAND | wxALL, 20);
+
+    install_button = new wxButton(this, ID_Install_Button, "Install", wxPoint(20, 20));
+    set_location = new wxButton(this, ID_Set_Location, "Set Location", wxPoint(20, 20));
+
+    mainSizer->AddStretchSpacer(); 
+    mainSizer->Add(install_button, 0, wxALIGN_CENTER | wxALL, 10); 
+    mainSizer->Add(set_location, 0, wxALIGN_CENTER | wxALL, 5);
+
+
+    SetSizerAndFit(mainSizer);
+
+
+
     Bind(wxEVT_MENU, &NewFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &NewFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &NewFrame::OnExit, this, wxID_EXIT);
+    Bind(wxEVT_BUTTON, &NewFrame::OnInstallButtonClick, this, ID_Install_Button);
+    Bind(wxEVT_BUTTON, &NewFrame::OnSetButtonClick, this, ID_Set_Location);
+
+
+    wxWindow::SetInitialSize(wxSize(700, 500));
 }
 
 void NewFrame::OnExit(wxCommandEvent& event)
@@ -73,4 +113,14 @@ void NewFrame::OnAbout(wxCommandEvent& event)
 void NewFrame::OnHello(wxCommandEvent& event)
 {
     wxLogMessage("Hello world from wxWidgets!");
+}
+
+void NewFrame::OnInstallButtonClick(wxCommandEvent& event)
+{
+    wxMessageBox("Button clicked!", "Button", wxOK | wxICON_INFORMATION);
+}
+
+void NewFrame::OnSetButtonClick(wxCommandEvent& event)
+{
+    wxMessageBox("Button clicked!", "Button", wxOK | wxICON_INFORMATION);
 }
