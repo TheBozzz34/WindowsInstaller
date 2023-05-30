b6094d74a7dbfebc38bc9b15f6a6cfb8deca0d64
Ethan James <lskywalker416@gmail.com>
2023-05-29 22:30:31 -0600

    Signing Test

--- WindowsInstaller/main.cpp
+++ WindowsInstaller/main.cpp
@@ -206,8 +206,6 @@ NewFrame::NewFrame()
 
 
 
-
-
     Bind(wxEVT_MENU, &NewFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &NewFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &NewFrame::OnExit, this, wxID_EXIT);
