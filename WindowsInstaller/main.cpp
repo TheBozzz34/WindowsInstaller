81f369e8759f8bff972cf8238f6c7a0257240ff5
Ethan James <lskywalker416@gmail.com>
2023-05-29 20:08:46 -0600

    Well, its a little better, and a little worse

--- WindowsInstaller/main.cpp
+++ WindowsInstaller/main.cpp
@@ -1,51 +1,143 @@
 #include <wx/wx.h>
 
-class WindowsInstaller : public wxApp
+#include <iostream>
+#include <fstream>
+#include <curl/curl.h>
+#include <thread>
+#include <filesystem>
+#include <boost/filesystem.hpp>
+
+
+
+class BinaryFileDownloader
 {
 public:
-    bool OnInit() override;
-};
+    BinaryFileDownloader(wxGauge* progressGauge)
+        : progressGauge_(progressGauge)
+    {
+    }
 
-// This defines the equivalent of main() for the current platform.
-wxIMPLEMENT_APP(WindowsInstaller);
+    bool DownloadFile(const std::string& url, const std::string& destination)
+    {
+        FILE* file = fopen(destination.c_str(), "wb");
+        if (!file)
+        {
+            std::cerr << "Failed to open destination file: " << destination << std::endl;
+            return false;
+        }
 
-class NewFrame : public wxFrame
-{
-public:
-   NewFrame();
+        CURL* curl = curl_easy_init();
+        if (!curl)
+        {
+            std::cerr << "Failed to initialize libcurl" << std::endl;
+            fclose(file);
+            return false;
+        }
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+        curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, ProgressCallback);
+        curl_easy_setopt(curl, CURLOPT_XFERINFODATA, this);
+
+        CURLcode res = curl_easy_perform(curl);
+        fclose(file);
+        curl_easy_cleanup(curl);
+
+        if (res != CURLE_OK)
+        {
+            std::cerr << "Failed to download file: " << curl_easy_strerror(res) << std::endl;
+            return false;
+        }
+
+        std::cout << "File downloaded successfully!" << std::endl;
+        return true;
+    }
+
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userdata)
+    {
+        FILE* file = static_cast<FILE*>(userdata);
+        size_t written = fwrite(contents, size, nmemb, file);
+        return written;
+    }
+
+    static int ProgressCallback(void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow)
+    {
+        BinaryFileDownloader* downloader = static_cast<BinaryFileDownloader*>(clientp);
+        if (downloader->progressGauge_)
+        {
+            if (dltotal > 0)
+            {
+                double progress = static_cast<double>(dlnow) / static_cast<double>(dltotal) * 100.0;
+                downloader->progressGauge_->SetValue(static_cast<int>(progress));
+            }
+        }
+        return 0;
+    }
 
 private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-    void OnInstallButtonClick(wxCommandEvent& event);
-    void OnSetButtonClick(wxCommandEvent& event);
-
-    wxButton* install_button;
-    wxButton* set_location;
-
-    wxTextCtrl* textCtrl;
+    wxGauge* progressGauge_;
 };
-
-bool WindowsInstaller::OnInit()
-{
-    NewFrame* frame = new NewFrame();
-    frame->Show();
-    return true;
-}
-
 
 enum
 {
     ID_Hello = 1,
     ID_Install_Button = 2,
-    ID_Set_Location = 3
+    ID_Set_Location = 3,
+    ID_Accept_CheckBox = 4,
+    ID_UPDATE_PROGRESS = 5,
+    ID_DOWNLOAD_COMPLETE = 6,
+    ID_DOWNLOAD_ERROR = 7,
+    ID_ENABLE_INSTALL_BUTTON = 8
 
 };
+
+class NewFrame : public wxFrame
+{
+public:
+    NewFrame();
+    void OnHello(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    void OnInstallButtonClick(wxCommandEvent& event);
+    void OnSetButtonClick(wxCommandEvent& event);
+    void OnCheckBoxClicked(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+
+
+
+private:
+    wxButton* install_button;
+    wxButton* set_location;
+    wxTextCtrl* textCtrl;
+    wxTextCtrl* statusCtrl;
+    wxCheckBox* accept_checkbox;
+    wxGauge* gauge;
+};
+
+class WindowsInstallerApp : public wxApp
+{
+public:
+    bool OnInit() override;
+    int OnExit() override;
+
+private:
+    NewFrame* frame;
+};
+
+wxIMPLEMENT_APP(WindowsInstallerApp);
+
+bool WindowsInstallerApp::OnInit()
+{
+    frame = new NewFrame();
+    frame->Show(true);
+    return true;
+}
 
 
 NewFrame::NewFrame()
     : wxFrame(nullptr, wxID_ANY, "Hello World", wxDefaultPosition, wxSize(700, 500), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
+
 {
     wxMenu* menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl+H",
@@ -63,7 +155,7 @@ NewFrame::NewFrame()
     SetMenuBar(menuBar);
 
     CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+    SetStatusText("Installer Ready!");
 
     wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
 
@@ -72,13 +164,38 @@ NewFrame::NewFrame()
     textCtrl->SetForegroundColour(*wxBLACK);
     textCtrl->SetFont(wxFont(wxFontInfo(10)));
 
-    wxString licenseText = "Copyright 2023 Ethan James \n\n Permission is hereby granted, free of charge, to any person obtaining a copy of this software and \n associated documentation files(the “Software”), to deal in the Software without restriction, \n including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, \n and /or sell copies of the Software, and to permit persons to whom the Software is furnished to do \n so, subject to the following conditions : \n The above copyright notice and this permission notice shall be included in all copies or substantial \n portions of the Software. \n\n THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR \n IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS \n FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS \n OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, \n WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN \n CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+    wxString licenseText = "Copyright 2023 Ethan James \n\n"
+        "Permission is hereby granted, free of charge, to any person obtaining a copy of this software "
+        "and \nassociated documentation files(the “Software”), to deal in the Software without restriction, "
+        "\nincluding without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, "
+        "\nand/or sell copies of the Software, and to permit persons to whom the Software is furnished to do \n"
+        "so, subject to the following conditions : \n"
+        "The above copyright notice and this permission notice shall be included in all copies or substantial \n"
+        "portions of the Software. \n\n"
+        "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR \n"
+        "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS \n"
+        "FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS \n"
+        "OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, \n"
+        "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN \n"
+        "CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
     textCtrl->SetValue(licenseText);
+
+
 
     mainSizer->Add(textCtrl, 1, wxEXPAND | wxALL, 20);
 
+    accept_checkbox = new wxCheckBox(this, ID_Accept_CheckBox, "I accept the license agreement");
+    mainSizer->Add(accept_checkbox, 0, wxALIGN_LEFT | wxALL, 10);
+
+    gauge = new wxGauge(this, wxID_ANY, 100, wxDefaultPosition, wxDefaultSize, wxGA_HORIZONTAL);
+    mainSizer->Add(gauge, 0, wxEXPAND | wxALL, 20);
+    gauge->SetValue(0);
+
+
     install_button = new wxButton(this, ID_Install_Button, "Install", wxPoint(20, 20));
     set_location = new wxButton(this, ID_Set_Location, "Set Location", wxPoint(20, 20));
+
+    install_button->Enable(false);
 
     mainSizer->AddStretchSpacer(); 
     mainSizer->Add(install_button, 0, wxALIGN_CENTER | wxALL, 10); 
@@ -89,19 +206,17 @@ NewFrame::NewFrame()
 
 
 
+
+
     Bind(wxEVT_MENU, &NewFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &NewFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &NewFrame::OnExit, this, wxID_EXIT);
     Bind(wxEVT_BUTTON, &NewFrame::OnInstallButtonClick, this, ID_Install_Button);
     Bind(wxEVT_BUTTON, &NewFrame::OnSetButtonClick, this, ID_Set_Location);
+    Bind(wxEVT_CHECKBOX, &NewFrame::OnCheckBoxClicked, this, ID_Accept_CheckBox);
 
 
     wxWindow::SetInitialSize(wxSize(700, 500));
-}
-
-void NewFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
 }
 
 void NewFrame::OnAbout(wxCommandEvent& event)
@@ -112,15 +227,65 @@ void NewFrame::OnAbout(wxCommandEvent& event)
 
 void NewFrame::OnHello(wxCommandEvent& event)
 {
-    wxLogMessage("Hello world from wxWidgets!");
+    
 }
 
 void NewFrame::OnInstallButtonClick(wxCommandEvent& event)
 {
-    wxMessageBox("Button clicked!", "Button", wxOK | wxICON_INFORMATION);
+    if (!accept_checkbox->IsChecked())
+    {
+        wxMessageBox("Please accept the license agreement.", "Error", wxOK | wxICON_ERROR);
+        return;
+    }
+
+    install_button->Enable(false);
+    accept_checkbox->Enable(false);
+
+    BinaryFileDownloader downloader(gauge);
+    std::string url = "https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso";
+    std::string destination = "F:/ubuntu-22.04.2-desktop-amd64.iso";
+    const std::string filePath = destination;
+    if (boost::filesystem::exists(filePath)) {
+        
+    }
+    else {
+        
+    }
+    bool success = downloader.DownloadFile(url, destination);
+    if (success)
+    {
+        
+    }
+    else
+    {
+        wxMessageBox("There was an error downloading required files", "Error", wxOK | wxICON_ERROR);
+      
+    }
 }
+
 
 void NewFrame::OnSetButtonClick(wxCommandEvent& event)
 {
     wxMessageBox("Button clicked!", "Button", wxOK | wxICON_INFORMATION);
 }
+
+void NewFrame::OnCheckBoxClicked(wxCommandEvent& event)
+{
+    if (accept_checkbox->IsChecked())
+        install_button->Enable(true);
+    else
+        install_button->Enable(false);
+}
+
+int WindowsInstallerApp::OnExit()
+{
+    delete frame;
+    return wxApp::OnExit();
+}
+
+
+void NewFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+
