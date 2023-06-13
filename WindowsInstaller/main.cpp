df3f36db42219ad375dc19c9de25067ef80f446c
Ethan James <necrozma@catgirlsaresexy.org>
2023-06-12 20:26:09 -0600

    shit and studff

--- WindowsInstaller/main.cpp
+++ WindowsInstaller/main.cpp
@@ -1,4 +1,5 @@
 #include <wx/wx.h>
+#include <wx/filepicker.h>
 
 #include <iostream>
 #include <fstream>
@@ -8,7 +9,14 @@
 #include <future>
 
 
-class BinaryFileDownloader
+
+/*
+    This is a simple installer for Windows. It will download a binary file from a URL and install it to a specified location.
+    The installer will also create a desktop shortcut to the installed file.
+*/
+
+
+class BinaryFileDownloader  // This class is used to download a binary file from a URL, it its incredibly janky and I would not recommend using it.
 {
 public:
     BinaryFileDownloader(wxGauge* progressGauge)
@@ -21,7 +29,7 @@ public:
         FILE* file = fopen(destination.c_str(), "wb");
         if (!file)
         {
-            std::cerr << "Failed to open destination file: " << destination << std::endl;
+            std::cerr << "Failed to open destination file: " << destination << std::endl; // wtf this is not a console app lmao, idk why this is here
             return false;
         }
 
@@ -33,7 +41,7 @@ public:
             return false;
         }
 
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // goofy cURL stuff
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
@@ -57,7 +65,7 @@ public:
     static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userdata)
     {
         FILE* file = static_cast<FILE*>(userdata);
-        size_t written = fwrite(contents, size, nmemb, file);
+        size_t written = fwrite(contents, size, nmemb, file);  // write the file probably
         return written;
     }
 
@@ -69,14 +77,14 @@ public:
             if (dltotal > 0)
             {
                 double progress = static_cast<double>(dlnow) / static_cast<double>(dltotal) * 100.0;
-                downloader->progressGauge_->SetValue(static_cast<int>(progress));
+                downloader->progressGauge_->SetValue(static_cast<int>(progress));  // update the progress bar
             }
         }
         return 0;
     }
 
 private:
-    wxGauge* progressGauge_;
+    wxGauge* progressGauge_;  // why is this here lmao
 };
 
 enum
@@ -88,7 +96,8 @@ enum
     ID_UPDATE_PROGRESS = 5,
     ID_DOWNLOAD_COMPLETE = 6,
     ID_DOWNLOAD_ERROR = 7,
-    ID_ENABLE_INSTALL_BUTTON = 8
+    ID_ENABLE_INSTALL_BUTTON = 8,
+    ID_DIRPICKERCTRL = 9,
 
 };
 
@@ -103,7 +112,7 @@ public:
     void OnCheckBoxClicked(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnDownloadComplete(wxThreadEvent& event);
-
+    void OnDirPickerChanged(wxFileDirPickerEvent& event);
 
 private:
     wxButton* install_button;
@@ -111,8 +120,12 @@ private:
     wxTextCtrl* textCtrl;
     wxTextCtrl* statusCtrl;
     wxCheckBox* accept_checkbox;
+    wxStaticText* label;
     wxGauge* gauge;
+    wxDirPickerCtrl* dirPickerCtrl;
     std::thread downloadThread;
+    std::string downloadUrl;
+    // sizers should be here
 };
 
 class WindowsInstallerApp : public wxApp
@@ -125,18 +138,18 @@ private:
     NewFrame* frame;
 };
 
-wxIMPLEMENT_APP(WindowsInstallerApp);
+wxIMPLEMENT_APP(WindowsInstallerApp); // this defines the main entry point for the application
 
 bool WindowsInstallerApp::OnInit()
 {
     frame = new NewFrame();
-    frame->Show(true);
+    frame->Show(true); // show the window
     return true;
 }
 
 
 NewFrame::NewFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World", wxDefaultPosition, wxSize(700, 500), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
+    : wxFrame(nullptr, wxID_ANY, "Hello World", wxDefaultPosition, wxSize(700, 500), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX)) // create the window and set its size and title
 
 {
     wxMenu* menuFile = new wxMenu;
@@ -164,6 +177,8 @@ NewFrame::NewFrame()
     textCtrl->SetForegroundColour(*wxBLACK);
     textCtrl->SetFont(wxFont(wxFontInfo(10)));
 
+
+    // This is a shitty way to do this but I don't care
     wxString licenseText = "Copyright 2023 Ethan James \n\n"
         "Permission is hereby granted, free of charge, to any person obtaining a copy of this software "
         "and \nassociated documentation files(the “Software”), to deal in the Software without restriction, "
@@ -180,8 +195,6 @@ NewFrame::NewFrame()
         "CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
     textCtrl->SetValue(licenseText);
 
-
-
     mainSizer->Add(textCtrl, 1, wxEXPAND | wxALL, 20);
 
     accept_checkbox = new wxCheckBox(this, ID_Accept_CheckBox, "I accept the license agreement");
@@ -191,19 +204,39 @@ NewFrame::NewFrame()
     mainSizer->Add(gauge, 0, wxEXPAND | wxALL, 20);
     gauge->SetValue(0);
 
-
     install_button = new wxButton(this, ID_Install_Button, "Install", wxPoint(20, 20));
-    set_location = new wxButton(this, ID_Set_Location, "Set Location", wxPoint(20, 20));
+    // set_location = new wxButton(this, ID_Set_Location, "Set Location", wxPoint(20, 20));
+
+    /* idk why i did this with a textctrl and not a statictext
+    wxString locationLabel = "Install Location: ";
+    locationLabelCtrl = new wxTextCtrl(this, wxID_ANY, locationLabel, wxDefaultPosition, wxDefaultSize, wxTE_READONLY);
+    locationLabelCtrl->SetBackgroundColour(*wxWHITE);
+    locationLabelCtrl->SetForegroundColour(*wxBLACK);
+    locationLabelCtrl->SetFont(wxFont(wxFontInfo(10)));
+    mainSizer->Add(locationLabelCtrl, 0, wxEXPAND | wxALL, 20);
+    */
+
+    wxBoxSizer* horizontalSizer = new wxBoxSizer(wxHORIZONTAL); // create a sizer to hold the buttons, not sure why the variable isnt with the rest of the variables in the class definition
+    label = new wxStaticText(this, wxID_ANY, "Select an install folder: ");
+    horizontalSizer->Add(label, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+    dirPickerCtrl = new wxDirPickerCtrl(this, ID_DIRPICKERCTRL, wxEmptyString, wxDirSelectorPromptStr, wxDefaultPosition, wxDefaultSize, wxDIRP_DIR_MUST_EXIST | wxDIRP_USE_TEXTCTRL);
+    horizontalSizer->Add(dirPickerCtrl, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
+
+    // Add the horizontal sizer to the main sizer
+    mainSizer->Add(horizontalSizer, 0, wxALIGN_CENTER | wxALL, 20);
 
     install_button->Enable(false);
+    dirPickerCtrl->Enable(false);
 
     mainSizer->AddStretchSpacer();
     mainSizer->Add(install_button, 0, wxALIGN_CENTER | wxALL, 10);
-    mainSizer->Add(set_location, 0, wxALIGN_CENTER | wxALL, 5);
+    // mainSizer->Add(set_location, 0, wxALIGN_CENTER | wxALL, 5);
 
     SetSizerAndFit(mainSizer);
 
 
+    // Connect event handlers
     Bind(wxEVT_MENU, &NewFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &NewFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &NewFrame::OnExit, this, wxID_EXIT);
@@ -211,7 +244,9 @@ NewFrame::NewFrame()
     Bind(wxEVT_BUTTON, &NewFrame::OnSetButtonClick, this, ID_Set_Location);
     Bind(wxEVT_CHECKBOX, &NewFrame::OnCheckBoxClicked, this, ID_Accept_CheckBox);
     Bind(wxEVT_THREAD, &NewFrame::OnDownloadComplete, this, ID_DOWNLOAD_COMPLETE);
+    Bind(wxEVT_DIRPICKER_CHANGED, &NewFrame::OnDirPickerChanged, this, ID_DIRPICKERCTRL);
 
+        
 
     wxWindow::SetInitialSize(wxSize(700, 500));
 }
@@ -224,7 +259,7 @@ void NewFrame::OnAbout(wxCommandEvent& event)
 
 void NewFrame::OnHello(wxCommandEvent& event)
 {
-
+    // wxLogMessage("Hello world from wxWidgets!");
 }
 
 void NewFrame::OnInstallButtonClick(wxCommandEvent& event)
@@ -236,14 +271,14 @@ void NewFrame::OnInstallButtonClick(wxCommandEvent& event)
     }
 
     install_button->Enable(false);
-    accept_checkbox->Enable(false);
-    set_location->Enable(false);
+    accept_checkbox->Enable(false); // disable the checkbox and stuff
+    dirPickerCtrl->Enable(false);
 
     
     downloadThread = std::thread([this]() {
          BinaryFileDownloader downloader(gauge);
          std::string url = "https://releases.ubuntu.com/22.04.2/ubuntu-22.04.2-desktop-amd64.iso";
-         std::string destination = "F:/ubuntu-22.04.2-desktop-amd64.iso";
+         std::string destination = downloadUrl + "\\ubuntu-22.04.2-desktop-amd64.iso";
          bool success = downloader.DownloadFile(url, destination);
 
          // Notify the main thread when the download is complete.
@@ -251,19 +286,11 @@ void NewFrame::OnInstallButtonClick(wxCommandEvent& event)
          event->SetString(success ? "Download successful" : "Download failed");
          wxQueueEvent(this, event);
      });
-   
-    
-
     
 }
 
 void NewFrame::OnDownloadComplete(wxThreadEvent& event)
 {
-    /*
-    std::string message = event.GetString();
-    std::cout << message << std::endl;
-    */
-
     // Cleanup the download thread
     if (downloadThread.joinable()) {
         downloadThread.join();
@@ -273,27 +300,37 @@ void NewFrame::OnDownloadComplete(wxThreadEvent& event)
 
 void NewFrame::OnSetButtonClick(wxCommandEvent& event)
 {
-    wxMessageBox("Button clicked!", "Button", wxOK | wxICON_INFORMATION);
+    
 }
 
 void NewFrame::OnCheckBoxClicked(wxCommandEvent& event)
 {
     if (accept_checkbox->IsChecked())
-        install_button->Enable(true);
+    {
+		install_button->Enable(true);
+        dirPickerCtrl->Enable(true);
+	}
     else
-        install_button->Enable(false);
+    {
+		install_button->Enable(false);
+        dirPickerCtrl->Enable(false);
+	}
+}
+
+void NewFrame::OnDirPickerChanged(wxFileDirPickerEvent& event)
+{
+    downloadUrl = event.GetPath();
+	// locationLabelCtrl->SetValue(path);
 }
 
 int WindowsInstallerApp::OnExit()
 {
-
-    delete frame;
+    delete frame; // frame is really a NewFrame
     return wxApp::OnExit();
 }
 
 
 void NewFrame::OnExit(wxCommandEvent& event)
 {
-
-    Close(true);
+    Close(true); // true is to force the frame to close
 }
